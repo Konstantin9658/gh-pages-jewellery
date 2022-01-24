40433d325874360a319b61f2087bf28efbb832f4
Konstantin <Super.Juke88@yandex.ru>
2022-01-24 16:25:30 +0400

    test

--- js/main.js
+++ js/main.js
@@ -1,161 +1,248 @@
 'use strict';
 (function () {
 
-  // POPUP
+  const hideList = (item, className) => {
+    item.classList.remove(className);
+  }
 
-  const formTemplate = document.querySelector('#popup').content.querySelector('.modal');
-  const btnCallback = document.querySelector('.button--callback');
+  const showList = (item, className) => {
+    item.classList.add(className);
+  }
+
+  // MENU
+
+  const body = document.querySelector('.page__body');
+  const header = document.querySelector('.header');
+  const intro = header.querySelector('.header__intro');
+  const btnMenu = header.querySelector('.button');
+  const headerWrap = header.querySelector('.header__wrapper');
+  const menu = header.querySelector('.header__nav-menu');
   const overlay = document.querySelector('.overlay');
+  const accordionMain = document.querySelector('.accordion');
+  const accordionFilter = document.querySelector('.accordion-filter');
+  const login = menu.querySelector('#login')
+
+  headerWrap.classList.remove('header__wrapper--no-js');
+  menu.classList.remove('header__nav-menu--no-js');
+  intro.classList.remove('header__intro--no-js');
+
+  if (!accordionFilter) {
+    accordionMain.classList.remove('accordion--no-js');
+  } else {
+    accordionFilter.classList.remove('accordion-filter--no-js');
+  }
 
   const isEscEvent = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
 
-  const closePopup = (popup) => {
-    popup.remove();
-    document.body.style.overflow = '';
+  document.addEventListener('keydown', (evt) => {
+    if (isEscEvent(evt)) {
+      evt.preventDefault();
+      hideMenu(btnMenu, headerWrap, menu);
+      setOverlayHide(overlay);
+    }
+  });
+
+  btnMenu.addEventListener('click', () => {
+    if (btnMenu.classList.contains('button--burger')) {
+      showMenu(btnMenu, headerWrap, menu);
+      setOverlayVisible(overlay);
+    } else {
+      hideMenu(btnMenu, headerWrap, menu);
+      setOverlayHide(overlay);
+    }
+  })
+
+  function setOverlayVisible(overlay) {
+    body.style.overflow = 'hidden';
+    overlay.classList.remove('overlay--hidden');
+  }
+
+  function setOverlayHide(overlay) {
+    body.style.overflow = '';
     overlay.classList.add('overlay--hidden');
   }
 
-  const showFormCallback = (formTemplate) => {
-    const btnFormClose = formTemplate.querySelector('.modal__btn');
-    const inputPhoneForm = formTemplate.querySelector('[name="modal-phone"]');
-    const btnFormSubmit = formTemplate.querySelector('.button--modal');
+  function showMenu(btn, wrapper, menu) {
+    btn.classList.remove('button--burger');
+    btn.classList.add('button--closed');
+    wrapper.classList.add('header__wrapper--open-menu');
+    menu.classList.add('header__nav-menu--show');
+  }
 
-    document.body.insertAdjacentElement('beforeend', formTemplate);
-    document.body.style.overflow = 'hidden';
+  function hideMenu(btn, wrapper, menu) {
+    btn.classList.add('button--burger');
+    btn.classList.remove('button--closed');
+    wrapper.classList.remove('header__wrapper--open-menu');
+    menu.classList.remove('header__nav-menu--show');
+  }
 
-    overlay.classList.remove('overlay--hidden');
+  // POPUP
 
-    let name = formTemplate.querySelector('#modal-name-id');
-    let phone = formTemplate.querySelector('#modal-phone-id');
-    let message = formTemplate.querySelector('#modal-question-id');
-    name.focus();
+  const formTemplate = document.querySelector('#popup').content.querySelector('.modal-login');
+  const btnLogin = document.querySelector('.header__login');
 
-    let isStorageSupport = true;
-    let storageName = '';
-    let storagePhone = '';
-    let storageMessage = '';
+  const closePopup = (popup) => {
+    popup.remove();
+    setOverlayHide(overlay);
+  }
 
-    try {
-      storageName = localStorage.getItem('name');
-      storagePhone = localStorage.getItem('phone');
-      storageMessage = localStorage.getItem('message');
-    } catch (err) {
-      isStorageSupport = false;
-    }
+  const showFormLogin = (template) => {
+    const btnFormClose = template.querySelector('.modal-login__button');
 
-    if (storageName) {
-      name.value = storageName;
-      phone.focus();
-    }
-    if (storagePhone) {
-      phone.value = storagePhone;
-      message.focus();
-    }
-    if (storageMessage) {
-      message.innerText = storageMessage;
-    } else {
-      name.focus();
-    }
+    document.body.insertAdjacentElement('beforeend', template);
+    setOverlayVisible(overlay);
 
-    btnFormSubmit.addEventListener('submit', function (evt) {
-      if (!name.value || !phone.value || !message.value) {
-        evt.preventDefault();
-      } else {
-        if (isStorageSupport) {
-          localStorage.setItem('name', name.value);
-          localStorage.setItem('phone', phone.value);
-          localStorage.setItem('message', message.value);
-        }
-      }
-    });
-    btnFormClose.addEventListener('click', () => closePopup(formTemplate), {once: true});
-    overlay.addEventListener('click', () => closePopup(formTemplate), {once: true});
-
-    validatePhone(inputPhoneForm);
+    btnFormClose.addEventListener('click', () => closePopup(template), {once: true});
+    overlay.addEventListener('click', () => closePopup(template), {once: true});
 
     document.addEventListener('keydown', (evt) => {
       if (isEscEvent(evt)) {
         evt.preventDefault();
-        closePopup(formTemplate);
+        closePopup(template);
       }
     }, {once: true});
   };
 
-  const showPopup = () => showFormCallback(formTemplate.cloneNode(true));
-  btnCallback.addEventListener('click', showPopup);
+  const showPopup = (evt) => {
+    evt.preventDefault();
+    showFormLogin(formTemplate.cloneNode(true));
+  }
+
+  btnLogin.addEventListener('click', showPopup);
+
+  login.addEventListener('click', (evt) => {
+    setOverlayVisible(overlay);
+    hideMenu(btnMenu, headerWrap, menu);
+    showPopup(evt);
+  });
 
   // = = = = = = = = = = = = = = = = = = = = = = = = = = = //
 
-  // FOOTER ACCORDION
+  if (accordionMain) {
+    // ACCORDION
 
-  const footerNavigation = document.querySelector('.footer__site-nav');
-  const buttonsOpenMenu = footerNavigation.querySelectorAll('.footer__btn');
-  const accordions = footerNavigation.querySelectorAll('.footer__accordion');
+    const accordionBtn = accordionMain.querySelectorAll('.accordion__toggle');
+    const accordionItem = accordionMain.querySelectorAll('.accordion__item');
 
-  accordions.forEach(list => list.classList.remove('footer__accordion--no-js'));
-  buttonsOpenMenu.forEach(btn => btn.classList.remove('footer__btn--no-js'));
+    for (let i = 0; i < accordionBtn.length; i++) {
+      accordionBtn[i].addEventListener('click', toggleItem, false);
+    }
 
-  for (let i = 0; i < buttonsOpenMenu.length; i++) {
-    buttonsOpenMenu[i].addEventListener('click', toggleItem, false);
-  }
+    function toggleItem() {
+      let item = this.parentElement;
 
-  const hideList = (list) => {
-    list.classList.remove('footer__accordion-open');
-    list.classList.add('footer__accordion-close');
-  }
+      if (!item.classList.contains('accordion__item--show')) {
+        accordionItem.forEach(item => hideList(item, 'accordion__item--show'));
+        showList(item, 'accordion__item--show');
+      } else {
+        hideList(item, 'accordion__item--show');
+      }
+    }
 
-  const showList = (list) => {
-    list.classList.add('footer__accordion-open');
-    list.classList.remove('footer__accordion-close');
-  }
+    // SLIDER
 
-  function toggleItem() {
-    let item = this.parentElement;
+    let position = 0;
+    let slidesToShow = 4;
+    let slidesToScroll = 4;
+    const container = document.querySelector('.product__container');
+    const track = document.querySelector('.product__cards');
+    const items = track.querySelectorAll('.product__card');
+    const itemsCount = items.length;
+    const itemWidth = container.clientWidth / slidesToShow;
+    const movePosition = slidesToScroll * itemWidth;
+    const btnNext = document.querySelector('.product__control-btn--right');
+    const btnPrev = document.querySelector('.product__control-btn--left');
 
-    if (item.classList.contains('footer__accordion-close')) {
-      accordions.forEach(list => hideList(list));
-      showList(item);
-    } else {
-      hideList(item);
+    // if (matchMedia) {
+    //   let screen = window.matchMedia('(max-width: 1023px)');
+    //   screen.addEventListener(changes);
+    //   changes(screen);
+    // }
+
+    // function changes(screen) {
+    //   if (screen.matches) {
+    //   slidesToShow = 2;
+    //   slidesToScroll = 2;
+    //   btnNext.addEventListener('click', btnNextHandler)
+    //   btnPrev.addEventListener('click', btnPrevHandler)
+    //   } else {
+    //   slidesToScroll = 4;
+    //   slidesToShow = 4;
+    //   btnNext.addEventListener('click', btnNextHandler)
+    //   btnPrev.addEventListener('click', btnPrevHandler)
+    //   }
+    // }
+
+    items.forEach(item => item.style.minWidth = `${itemWidth}px`);
+
+    function btnNextHandler() {
+      const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+      position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+
+      setPosition();
+      checkBtns();
+    }
+
+    function btnPrevHandler() {
+      const itemsLeft = Math.abs(position) / itemWidth;
+
+      position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+
+      setPosition();
+      checkBtns();
+    }
+
+    btnNext.addEventListener('click', btnNextHandler)
+    btnPrev.addEventListener('click', btnPrevHandler)
+
+    const setPosition = () => {
+      track.style.transform = `translateX(${position}px)`;
+    }
+
+    const checkBtns = () => {
+      btnPrev.disabled = position === 0;
+      btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+    }
+
+    checkBtns();
+  } else if (accordionFilter) {
+    const accordionFilterBtn = accordionFilter.querySelectorAll('.accordion-filter__btn');
+    const filterOpen = document.querySelector('.button--filter');
+    const filterContent = document.querySelector('.filter-box__inner');
+    const filterClose = document.querySelector('.button--filter-closed');
+    const filterApply = filterContent.querySelector('.button--apply');
+    const filterOverlay = document.querySelector('.filter-box__overlay');
+
+    filterOpen.addEventListener('click', showFilter)
+
+    filterClose.addEventListener('click', hideFilter)
+
+    filterApply.addEventListener('click', hideFilter)
+
+    filterOverlay.addEventListener('click', hideFilter)
+
+    function hideFilter() {
+      filterContent.classList.remove('filter-box__inner--show');
+      body.style.overflow = '';
+    }
+
+    function showFilter() {
+      filterContent.classList.add('filter-box__inner--show');
+      body.style.overflow = 'hidden';
+    }
+
+    for (let i = 0; i < accordionFilterBtn.length; i++) {
+      accordionFilterBtn[i].addEventListener('click', toggleItem, false);
+    }
+
+    function toggleItem() {
+      let item = this.parentElement;
+
+      if (!item.classList.contains('accordion-filter__item--show')) {
+        showList(item, 'accordion-filter__item--show');
+      } else {
+        hideList(item, 'accordion-filter__item--show');
+      }
     }
   }
-
-  // ANCHOR SCROLL
-
-  const anchors = document.querySelectorAll('a[href*="#"]');  //выбираем все ссылки к якорю на странице
-  const SPEED = 0.7;                                          // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
-  for (let i = 0; i < anchors.length; i++) {
-    anchors[i].addEventListener('click', function(evt) {
-      evt.preventDefault();
-      const scrollTo = window.pageYOffset;                  // производим прокрутку
-      const hash = this.href.replace(/[^#]*(.*)/, '$1');    // id элемента, к которому нужно перейти
-      const padding = document.querySelector(hash).getBoundingClientRect().top;  // отступ от окна браузера до id
-      let start = null;
-      requestAnimationFrame(step);
-      function step(time) {
-        if (start === null) start = time;
-        const progress = time - start;
-        const distance = (padding < 0 ? Math.max(scrollTo - progress/SPEED, scrollTo + padding) : Math.min(scrollTo + progress/SPEED, scrollTo + padding));
-        window.scrollTo(0, distance);
-        if (distance != scrollTo + padding) {
-          requestAnimationFrame(step)
-        } else {
-          location.hash = hash;
-        }
-      }
-    }, false);
-  }
-  // = = = = = = = = = = = = = = = = = = = = = = = = = = = //
-
-  // INPUT MASK FORM
-
-  const feedbackBlock = document.querySelector('.feedback')
-  const phoneInputForm = feedbackBlock.querySelector('#phone-form-field');
-
-  const validatePhone = function (element) {
-    const inputPhoneMask = new Inputmask('+7 (999) 999-99-99');
-    inputPhoneMask.mask(element);
-  };
-
-  validatePhone(phoneInputForm);
 })();
